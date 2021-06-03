e5ccd7a23927314e23b8ecbacd09e8211f6d8e33
Allen <allenchensheng@163.com>
2021-06-03 13:41:09 +0800

    serialization performance

--- interface/src/main/java/com/allen/SerializationOptimizerImpl.java
+++ interface/src/main/java/com/allen/SerializationOptimizerImpl.java
@@ -6,11 +6,17 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
+
+/**
+ * 序列化性能分析：https://dubbo.apache.org/zh/docs/v2.7/user/serialization/
+ */
+
 public class SerializationOptimizerImpl implements SerializationOptimizer {
     @Override
     public Collection<Class> getSerializableClasses() {
         List<Class> classes = new LinkedList<Class>();
         classes.add(PersonInfo.class);
+        classes.add(Object.class);
         return classes;
     }
 }
